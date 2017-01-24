6fb4b06f08a9181e1b55494bd782abb57d145cf6
Azure Rose Legacy and Brynna Klamkin-McCarter <brynna@>
2017-01-24 13:53:34 -0800

    add form content and initial UI logic

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+/////////////////////
+//Back-end
+/////////////////////
+var price = 0;
+//var userAge = new Age();
+
+// function Age(child = 6, adult = 12, senior = 10) {
+//   this.child = child;
+//   this.adult = adult;
+//   this.senior = senior;
+// }
+
+// function Ticket(title, times) {
+//   this.title = title;
+//   this.time = times;
+//   this.ages = [];
+// }
+
+// var lotb = new Ticket("Lord of the Bracelets", ["16:30", "19:00", "23:30"])
+//
+// Ticket.prototype.price = function(title, times, age) {
+
+//}
+
+
+/////////////////////
+//Front-end
+/////////////////////
+$(function() {
+  $("#userAge").hide();
+  $("#lotbTime").hide();
+  $("#hpTime").hide();
+  $("#sb3Time").hide();
+
+  $("#userMovie").submit();
+});
